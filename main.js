a2b6c34bbdbab6b9f0b7e97fd2aaa5e3d139da63
haitaodesign <1775814590@qq.com>
2017-09-10 19:38:55 +0800

    vuex,vue-bus

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './app.vue';
 
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const Routers = [
     {
@@ -51,23 +53,52 @@ const router = new VueRouter(RouterConfig);
 
 router.beforeEach((to,from,next)=>{
     // window.document.title = to.meta.title;
-    if(window.localStorage.getItem('token')){
-        next();
-    }else{
-        if(to.path=="/login"){
-            next();
-        }else{
-            next('/login');
-        }
-    }
+    // if(window.localStorage.getItem('token')){
+    //     next();
+    // }else{
+    //     if(to.path=="/login"){
+    //         next();
+    //     }else{
+    //         next('/login');
+    //     }
+    // }
+    next();
 });
 
 router.afterEach((to,from,next)=>{
-    window.scrollTo(0,0);
+    // window.scrollTo(0,0);
+    // next();
+});
+
+const store = new Vuex.Store({
+    // vuex 配置
+    state:{
+        count:0,
+        list:[1,39,4,5,66,7,9]
+    },
+    mutations:{                 // 尽量不要使用异步
+        increment(state){       //第二个额外参数，变量，对象
+            state.count++;          
+        },
+        decrease(state){
+            state.count--;
+        }
+    },
+    getters:{
+        filternumber:state => {
+            return state.list.filter(item => item < 10);
+        }
+    },
+    actions:{
+        increment(context){
+            context.commit('increment');
+        }
+    }
 });
 
 new Vue({
     el:'#app',
     router,
+    store,
     render:h=>h(App)
 });
--- view/index.vue
+++ view/index.vue
@@ -1,49 +1,12 @@
 <template>
   <div>首页
+      {{ $store.state.count}}
       <!-- active-class 跳转高亮 -->
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
+
       <router-link to="/about" active-class="classname">跳转到about</router-link>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
+        <button @click="handleclick">+1</button>
+        {{list}}
+        <button @click="handleAction">action +1</button>
   </div>
 </template>
 <script>
@@ -51,6 +14,20 @@ export default {
     data(){
         return{
 
+        }
+    },
+    computed:{
+        list(){
+            // return this.$store.state.list.filter(item => item < 10);
+            return this.$store.getters.filternumber;
+        }
+    },
+    methods:{
+        handleclick(){
+            this.$store.commit('increment'); // 在写一个减法
+        },
+        handleAction(){
+            this.$store.dispatch('increment');  // 通过dispatch 触发action
         }
     }
   
