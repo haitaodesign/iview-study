cbe3f17f4ef7d24ac15920ce916fdc9cebff6e91
haitaodesign <1775814590@qq.com>
2017-11-15 22:35:53 +0800

    插件演示完成

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,7 +29,7 @@ module.exports = merge(webpackBaseConfig,{
             }
         }),
         new HtmlWebpackPlugin({
-            filename:'../index_prod.html',
+            filename:'../dist/index_prod.html',
             template:'./index.ejs',
             inject:false
         })
